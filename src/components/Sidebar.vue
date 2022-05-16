b5de09c9b3f0fd319b9d0d412c399c1fe99e9c7a
Chris <chris.jol@gocadmium.com>
2022-05-16 15:08:18 -0700

    Update: add headshot

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,14 +2,9 @@
   <div
     class="border-r border-steel w-72 flex-shrink-0 px-8 py-6 flex flex-col gap-8"
   >
-    <div class="flex items-center gap-2">
-      <svg style="width: 30x; height: 30px" viewBox="0 0 24 24">
-        <path
-          fill="currentColor"
-          d="M19,19H5V8H19M16,1V3H8V1H6V3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3H18V1"
-        />
-      </svg>
-      <h1>Calendí</h1>
+    <div class="flex items-center gap-4">
+      <img class="w-12 rounded-full" src="@/assets/Headshot.jpg" />
+      <h2>Chris Jol</h2>
     </div>
 
     <button
